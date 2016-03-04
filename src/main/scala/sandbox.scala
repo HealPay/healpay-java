a349ecea49a5cd731fa45c97fbe6ad2fed1df12e
Lori Holden <email@loriholden.com>
2016-03-04 11:25:26 -0800

    Pass in the url from command line

--- src/main/scala/sandbox.scala
+++ src/main/scala/sandbox.scala
@@ -6,13 +6,13 @@ import healpay.payment.SoapGateway
 
 object Sandbox {
   def main(args: Array[String]): Unit = {
-    if (args.length != 2) {
+    if (args.length != 3) {
       println("You must specify a source_key and pin")
       return
     }
-    val url = "https://sandbox.usaepay.com/soap/gate/A47DE151"
-    val service_key = args(0)
-    val pin = args(1)
+    val url = args(0)
+    val service_key = args(1)
+    val pin = args(2)
     val service = new SoapGateway(url, service_key, pin)
     //val response = service.searchCustomers(List(("BillingAddress.lastname", "eq", "ASearchWithNoValidResults")), true)
     val response = Try(service.searchCustomers(List(("Enabled", "eq", "true")), true))
